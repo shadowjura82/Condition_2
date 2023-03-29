a32c493432cf636ba6c0ee54e2ed490a5ff2ba78
Yurii <shadowjura82@gmail.com>
2023-03-29 14:51:50 +0200

    task2 rework

--- src/Main.java
+++ src/Main.java
@@ -19,16 +19,14 @@ public class Main {
     public static void task2() {
         byte clientOS = 0;
         short clientDeviceYear = 2015;
-        if (clientOS == 0) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для iOS по ссылке\n");
-            } else System.out.println("Установите версию приложения для iOS по ссылке\n");
-        } else if (clientOS == 1) {
-            if (clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для Android по ссылке\n");
-            } else System.out.println("" +
-                    "Установите версию приложения для Android по ссылке\n");
-        } else System.out.println("Ваш телефон не поддерживает наше мобильное приложение\n");
+        if (clientDeviceYear < 2015 && clientOS == 0)
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке\n");
+        if (clientDeviceYear >= 2015 && clientOS == 0)
+            System.out.println("Установите версию приложения для iOS по ссылке\n");
+        if (clientDeviceYear < 2015 && clientOS == 1)
+            System.out.println("Установите облегченную версию приложения для Android по ссылке\n");
+        if (clientDeviceYear >= 2015 && clientOS == 1)
+            System.out.println("Установите версию приложения для Android по ссылке\n");
     }
 
     public static void task3() {
