bb78403acd5956cebd8dd554118cabfe38e6715d
Yurii <shadowjura82@gmail.com>
2023-03-29 12:32:55 +0200

    task2 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,29 @@
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static void task1() {
+        byte clientOS = 1;
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке\n");
+        } else if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке\n");
+        } else System.out.println("Ваш телефон не поддерживает наше мобильное приложение\n");
+    }
+
+    public static void task2() {
         byte clientOS = 0;
-        switch (clientOS) {
-            case 0:
-                System.out.println("Установите версию приложения для iOS по ссылке\n");
-                break;
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке\n");
-                break;
-            default:
-                System.out.println("Ваш телефон не поддерживает наше мобильное приложение\n");
-                break;
-        }
+        short clientDeviceYear = 2015;
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке\n");
+            } else System.out.println("Установите версию приложения для iOS по ссылке\n");
+        } else if (clientOS == 1) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке\n");
+            } else System.out.println("Установите версию приложения для Android по ссылке\n");
+        } else System.out.println("Ваш телефон не поддерживает наше мобильное приложение\n");
     }
 }
