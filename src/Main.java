7f05242aa055eb7107420d3c4d12b50b4a62f666
Yurii <shadowjura82@gmail.com>
2023-03-29 12:06:41 +0200

    task1 has been completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+    }
+
+    public static void task1() {
+        byte clientOS = 0;
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке\n");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке\n");
+                break;
+            default:
+                System.out.println("Ваш телефон не поддерживает наше мобильное приложение\n");
+                break;
+        }
     }
 }
