f8c3faf725828dda61b456bcab81d9fe3f138adc
Yurii <shadowjura82@gmail.com>
2023-03-29 13:54:43 +0200

    task3 has been completed

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
@@ -23,7 +24,19 @@ public class Main {
         } else if (clientOS == 1) {
             if (clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке\n");
-            } else System.out.println("Установите версию приложения для Android по ссылке\n");
+            } else System.out.println("" +
+                    "Установите версию приложения для Android по ссылке\n");
         } else System.out.println("Ваш телефон не поддерживает наше мобильное приложение\n");
+    }
+
+    public static void task3() {
+        short year = 2400;
+        if ((year % 400) == 0) {
+            System.out.println(year + " год является високосным\n");
+        } else if ((year % 4) == 0) {
+            if ((year % 100) == 0) {
+                System.out.println(year + " год не является високосным\n");
+            } else System.out.println(year + " год является високосным\n");
+        } else System.out.println(year + " год не является високосным\n");
     }
 }
